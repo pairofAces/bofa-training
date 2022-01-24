d05552781a0148c1e953bfc945b1c4013cce6640
pairofAces <karanchauhan16@gmail.com>
2022-01-24 14:26:38 -0500

    finalized solution to common_elements

--- Training/hands_on_problems/common_elements.py
+++ Training/hands_on_problems/common_elements.py
@@ -7,3 +7,19 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 def common_elements(a, b):
+    # convert the input lists into sets
+    set_a = set(a)
+    set_b = set(b)
+
+    # use if loop to compare sets (a) and (b)
+    if (set_a & set_b):
+        # cast the comparison between set_a and set_b as a (list)
+        # and print the common elements
+            # the (&) operator compares the sets and returns a set with the
+            # common elements, without duplicates
+        print(list(set_a & set_b))
+    else:
+        print("No common elements")
+
+
+common_elements(a, b)
