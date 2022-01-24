a53cfe6b1326a4d1d6b67dbe35b1e55c4bb78353
pairofAces <karanchauhan16@gmail.com>
2022-01-24 14:11:52 -0500

    creating solution for common elements between lists

---
+++ Training/hands_on_problems/common_elements.py
@@ -0,0 +1,9 @@
+# create a function that takes two lists and returns a list containing
+# the common elements between the initial two lists. 
+    # no duplicates
+    # make sure the program works with lists of different sizes
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def common_elements(a, b):
